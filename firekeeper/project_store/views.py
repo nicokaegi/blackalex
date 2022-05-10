51f2ad9a684e30d6be3327c157ffb2addab77b0c
nicokaegi <kaeginick63@gmail.com>
2022-05-10 15:29:40 -0400

    got the login page to work

--- firekeeper/project_store/views.py
+++ firekeeper/project_store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import FileResponse
+from django.contrib.auth.decorators import login_required
 
 import os
 
 # Create your views here.
-black_alex_path = "/blackalex"
+black_alex_path = "/home/sindri/blackalex"
 
 #app_name = 'project_store'
 
@@ -29,16 +30,19 @@ def break_into_3s(in_list):
     else:
         return []
 
+@login_required
 def home(request):
     list_of_dirs = os.listdir(black_alex_path)
     list_of_dirs = break_into_3s(list_of_dirs)
     return render(request, 'home.html', {'list_of_dirs' : list_of_dirs })
 
+@login_required
 def view_files(request, dir_name):
     dir_path = '/'.join([black_alex_path, dir_name])
     list_of_files = os.listdir(dir_path)
     return render(request, 'files.html', {'dir' : dir_name, 'list_of_files' : list_of_files})
 
+@login_required
 def download_file(request, dir_name, file_name):
     file_path = '/'.join([black_alex_path, dir_name, file_name])
     out_file = open(file_path,'rb')
